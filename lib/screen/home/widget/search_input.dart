ac48fef3ee7cf7d019eaed3ba20b2f37290c1461
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-01-19 21:44:43 +0700

    update search_input.dart

--- lib/screen/home/widget/search_input.dart
+++ lib/screen/home/widget/search_input.dart
@@ -7,18 +7,23 @@ class SearchInput extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-      decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(8), boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.3),
-          spreadRadius: 1,
-          blurRadius: 7,
-          offset: const Offset(0, 3),
-        )
-      ]),
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          )
+        ],
+      ),
       child: TextField(
         decoration: InputDecoration(
           prefixIcon: Container(
+            padding: const EdgeInsets.all(10),
             child: SvgPicture.asset(
               'assets/icons/search.svg',
               width: 24,
